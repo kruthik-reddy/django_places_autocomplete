7d763e3d4da0036d5e85d3dfc149ef84465b096e
Sai Kruthik Reddy Paduru <61983624+kruthik-reddy@users.noreply.github.com>
2025-08-04 00:36:33 -0400

    Add geocode view

--- django_places_autocomplete/addresses/urls.py
+++ django_places_autocomplete/addresses/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', views.address_form_view, name='address_form'),
     path('list/', views.address_list_view, name='address_list'),
+    path('geocode/', views.geocode_view, name='geocode'),
 ]
