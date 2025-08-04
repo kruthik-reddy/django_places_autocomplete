70d7ef0a034065a2037310f9d5b2b44a2c41747c
Sai Kruthik Reddy Paduru <61983624+kruthik-reddy@users.noreply.github.com>
2025-08-04 01:35:23 -0400

    Add geocode API view and route

--- django_places_autocomplete/addresses/urls.py
+++ django_places_autocomplete/addresses/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('', views.address_form_view, name='address_form'),
-    path('list/', views.address_list_view, name='address_list'),
-    path('geocode/', views.geocode_view, name='geocode'),
+    path("", views.address_form_view, name="address_form"),
+    path("list/", views.address_list_view, name="address_list"),
+    # API endpoint for forward and reverse geocoding
+    path("geocode/", views.geocode_view, name="geocode"),
 ]
